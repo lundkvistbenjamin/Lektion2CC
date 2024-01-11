848535de9b6fd7c581202efef648281449e7307e
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2024-01-11 14:01:48 +0200

    CC done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Shirt nikeShirt = new Shirt("Nike", "Black", 49.90);
+        System.out.println(("Brand: " + nikeShirt.getBrand()));
+        System.out.println(("Color: " + nikeShirt.getColor()));
+        System.out.println(("Price: " + nikeShirt.getPrice()));
+
+        Shirt pumaShirt = new Shirt("Puma", "Grey", 29.90);
+        System.out.println(("Brand: " + pumaShirt.getBrand()));
+        System.out.println(("Color: " + pumaShirt.getColor()));
+        System.out.println(("Price: " + pumaShirt.getPrice()));
     }
 }
---
+++ src/Shirt.java
@@ -0,0 +1,8 @@
+public class Shirt extends Clothing {
+
+    public Shirt(String brand, String color, double price) {
+        super.brand = brand;
+        super.color = color;
+        super.price = price;
+    }
+}
